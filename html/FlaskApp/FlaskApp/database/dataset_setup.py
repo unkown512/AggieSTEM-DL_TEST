36969e574ae22e45c2f2653cb1f3a9cfe4aaa61b
Luming Xu <xuluming95@gmail.com>
2020-03-23 10:06:31 -0500

    include table creation in setup

--- html/FlaskApp/FlaskApp/database/dataset_setup.py
+++ html/FlaskApp/FlaskApp/database/dataset_setup.py
@@ -11,6 +11,12 @@ with open(os.path.dirname(os.path.abspath(__file__))+"/user_info.json", 'r') as 
 
 db = pymysql.connect("localhost", user, password, "aggiestemdl")
 cursor = db.cursor()
+
+with open(os.path.dirname(os.path.abspath(__file__))+'/dataset_schema.sql', 'r') as f:
+    sql_querys = f.read().split('\n\n')
+    for query in sql_querys:
+        cursor.execute(query)
+
 with open(os.path.dirname(os.path.abspath(__file__))+'/seed_dataset.sql', 'r') as f:
     sql_querys = f.read().split('\n\n')
     for query in sql_querys:
