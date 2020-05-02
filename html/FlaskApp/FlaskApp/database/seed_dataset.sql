f892f721bf8d882a97f3ffaa1d28ba06fa20847f
Luming Xu <xuluming95@gmail.com>
2020-05-01 19:12:03 -0500

    Revert "update style"
    This reverts commit 455a7f015bc66e1c1c1fc4ee1365b80a5c5b44f4.

--- html/FlaskApp/FlaskApp/database/seed_dataset.sql
+++ html/FlaskApp/FlaskApp/database/seed_dataset.sql
@@ -6,17 +6,17 @@ from dataset_access;
 delete
 from dataset;
 
-insert into dataset (name, description, upload_time, update_time, filepath)
-values ('MNIST', 'handwritten digit images', '2020/03/22 10:03:01', '2020/03/22 10:03:01', 'static/txt/demo.txt');
+insert into dataset (name, description, upload_time, update_time)
+values ('MNIST', 'handwritten digit images', '2020/03/22 10:03:01', '2020/03/22 10:03:01');
 
-insert into dataset (name, description, upload_time, update_time, filepath)
-VALUES ('MS-COCO', 'object detection', '2020/03/01 17:10:15', '2020/03/01 17:10:15', 'static/csv/demo.csv');
+insert into dataset (name, description, upload_time, update_time)
+VALUES ('MS-COCO', 'object detection', '2020/03/01 17:10:15', '2020/03/01 17:10:15');
 
-insert into dataset (name, description, upload_time, update_time, filepath)
-VALUES ('CIFAR-10', 'image classification dataset', '2020/02/01 15:21:03', '2020/03/01 17:21:04', 'static/pdf/demo.pdf');
+insert into dataset (name, description, upload_time, update_time)
+VALUES ('CIFAR-10', 'image classification dataset', '2020/02/01 15:21:03', '2020/03/01 17:21:04');
 
-insert into dataset (name, description, upload_time, update_time, filepath)
-VALUES ('IMDB Reviews', 'IMDB movie reviews dataset', '2019/01/03 17:30:05', '2020/03/05 19:20:33', 'static/img/demo1.jpg');
+insert into dataset (name, description, upload_time, update_time)
+VALUES ('IMDB Reviews', 'IMDB movie reviews dataset', '2019/01/03 17:30:05', '2020/03/05 19:20:33');
 
 
 insert into dataset_access (user_id, dataset_id, status)
